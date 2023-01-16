e99dc28ef5516b9f627c64648dc491dd5b429dda
Kingston Vastine <kingstongise@gmail.com>
2023-01-16 15:15:27 -0500

    Fully parse email from every remind class.

--- main.py
+++ main.py
@@ -29,13 +29,8 @@ def getEmails(messageBytes):
 
     return msgs
 
-def parseMail(message):
-    # body = str()
-    # date = str()
-    # time = str()
-    # teacher = str()
-
-    # A regex to find the flags which will precede the body of our message,
+def extractBody(message):
+    # Regular Expressions to get the beginning and end positions of the email body.
     startRE = re.compile(r'Content-Type: text\/plain;.*\sMime-Version: \d\.\d\s\s')
     endRE = re.compile(r'--.*\n')
 
@@ -46,14 +41,49 @@ def parseMail(message):
     # Since the second match works on a smaller substring, we need to offset the position value.
     pos2 += pos1
 
-    print(message[pos1:pos2].strip())
+    return message[pos1:pos2].strip()
+
+def extractTime(message):
+    # A regular expression which matches Gmail's date/time formatting.
+    timeRE = re.compile(r'\w{3}, \d\d \w{3} \d{4} \d\d:\d\d:\d\d')
+    full = timeRE.search(message).group()
+
+    # Convert the PST timestamp to EST by adding 3 hours
+    timeEST = int(full[17:19]) + 3
+
+    # Indexing hijinks to reformat from "Mon, 16 Jan 2023 13:55:15" to "Mon, Jan 16 1:55 PM"
+    if timeEST > 12:
+        return "On " + full[:4] + full[7:11] + full[4:8] + str(timeEST - 12) + full[19:22] + " PM"
+    else:
+        return "On " + full[:4] + full[7:11] + full[4:8] + str(timeEST) + full[19:22] + " AM"
+
+def extractClass(message):
+    # A regular expression to grab the subject line of the email, detailing the class & teacher.
+    classRE = re.compile(r'(?<=Subject:) .*')
+    # The "17" index is there to remove the "new message from" text which proceeds the subject.
+    # The "-6" index is there to remove the "class" text which is ends the subject line.
+    full = classRE.search(message).group().strip()[17:-6]
+    return "From " + full
+def parseMail(message):
+    body = str()
+    time = str()
+    teacher = str()
+
+    body = extractBody(message)
+    time = extractTime(message)
+    teacher = extractClass(message)
+    print(teacher)
+    print(time)
+    print(body)
+    print()
 
 
 con = imaplib.IMAP4_SSL(imap_url)
 con.login(user, passwd)
 con.select("Inbox")
 
-messages = getEmails(search('FROM', 'chat+1ac362f0-7ba9-4dfb-9b15-27630bf5eb76@mail.remind.com', con))
+# Search for every email with "New Message" in the subject.
+messages = getEmails(search('SUBJECT', 'New Message', con))
 
 for msg in messages[::-1]:
     for sent in msg:
